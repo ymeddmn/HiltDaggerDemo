528e69c4b62c0dc5a249b75d44578fad423fa2da
mage <1275054668@qq.com>
2021-05-27 21:43:46 +0800

    较复杂一些的实现，Car中要注入Driver对象

--- app/src/main/java/com/mage/hiltdaggerdemo/Car.kt
+++ app/src/main/java/com/mage/hiltdaggerdemo/Car.kt
@@ -7,8 +7,9 @@ import javax.inject.Inject
  * function:
  * date    :2021/5/27
  **/
-class Car @Inject constructor() {
-    fun drive(name: String) {
-        println("老司机$name 在线开车")
+class Car @Inject constructor(val driver: Driver) {
+    //Car的构造函数中增加了Driver司机对象的实现
+    fun drive() {
+        println("老司机${driver.name} 在线开车")
     }
 }
---
+++ app/src/main/java/com/mage/hiltdaggerdemo/Driver.kt
@@ -0,0 +1,12 @@
+package com.mage.hiltdaggerdemo
+
+import javax.inject.Inject
+
+/**
+ * author  :mayong
+ * function:
+ * date    :2021/5/27
+ **/
+interface Driver {
+    val name :String
+}
